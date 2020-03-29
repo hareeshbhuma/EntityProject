04d15feffa3983f24b9e2c71ed9478a51092da8a
hareeshbhuma <hareeshbhuma@gmail.com>
2020-03-28 23:49:19 -0500

    SampleEntityProject

---
+++ DBSample/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using DBSample.DAL;
+using DBSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DBSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            SchoolContext school = new SchoolContext();
+            //Student s = new Student();
+            //s.FirstMidName = "gg";
+            //s.LastName = "j";
+            //s.ID = 1;
+            //s.EnrollmentDate = DateTime.Parse("2005-09-01");
+            //school.Students.Add(s);
+            //school.SaveChanges();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DBSample/DAL/SchoolContext.cs
@@ -0,0 +1,29 @@
+﻿using DBSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace DBSample.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolContext")
+        {
+            Database.SetInitializer<SchoolContext>(new CreateDatabaseIfNotExists<SchoolContext>());
+            //Database.SetInitializer<SchoolContext>(new DropCreateDatabaseIfModelChanges<SchoolContext>());
+            //Database.SetInitializer(new DropCreateDatabaseAlways<SchoolContext>());
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
